c8c9cda016f568dcf6064c81c74ceef6c7085bba
朱浩 <hao.zhu@shanbay.com>
2019-05-20 18:10:56 +0800

    feat: add token && split play and publish

--- app.js
+++ app.js
@@ -3,28 +3,63 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-app.get('/auth', function(req, res) {
+global.prikey = 81;
+global.playKey = 0;
+global.publishKey = 0;
+app.get('/publish_auth_rtmp', function(req, res) {
     console.log('query', req.query);
-    const name = req.body && req.query.name;
-    const pswd = req.body && req.query.key;
-    if (name === 'jiayue' && pswd === 'henry123') {
+    const name = req.query && req.query.name;
+    const key = req.query && req.query.key;
+    if (name === 'jiayue' && key === global.playKey) {
         res.sendStatus(200);
-    } else if (name === 'jiayue1' && pswd === 'henry123') {
+    } else if (name === 'jiayue1' && key === global.playKey) {
         res.sendStatus(200);
     } else {
         res.sendStatus(400);
     }
 });
-app.post('/auth', function(req, res) {
-    const name = req.body && req.body.name;
-    const pswd = req.body && req.body.key;
+app.get('/play_auth_rtmp', function(req, res) {
+    const name = req.query && req.query.name;
+    const key = req.query && req.query.key;
     console.log('req', req.body);
-    if (name === 'jiayue' && pswd === 'henry123') {
+    if (name === 'jiayue' && key === global.playKey) {
         res.sendStatus(200);
-    } else if (name === 'jiayue1' && pswd === 'henry123') {
+    } else if (name === 'jiayue1' && key === global.playKey) {
         res.sendStatus(200);
     } else {
         res.sendStatus(400);
+    }
+});
+
+app.post('/play_auth', function(req, res) {
+    const username = req.body && req.body.username;
+    const password = req.body && req.body.password;
+    const now = new Date();
+    const start = new Date(now.getFullYear(), 0, 0);
+    const diff = now - start;
+    const oneDay = 1000 * 60 * 60 * 24;
+    const day = Math.floor(diff / oneDay);
+    if (username === 'henry' && (password ^ global.prikeyay) === 44332211) {
+        global.playKey = 44332211 ^ day;
+        res.send(200, {
+            key = global.playKey,
+        });
+    }
+});
+
+app.post('/publish_auth', function(req, res) {
+    const username = req.body && req.body.username;
+    const password = req.body && req.body.password;
+    const now = new Date();
+    const start = new Date(now.getFullYear(), 0, 0);
+    const diff = now - start;
+    const oneDay = 1000 * 60 * 60 * 24;
+    const day = Math.floor(diff / oneDay);
+    if (username === 'henry' && (password ^ global.prikeyay) === 11223344) {
+        global.publishKey = 11223344 ^ day;
+        res.send(200, {
+            key = global.publishKey,
+        });
     }
 });
 
